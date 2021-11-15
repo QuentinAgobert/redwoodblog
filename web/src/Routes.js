1b5d3473753e53075aa50898808472dac2e9e07a
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2021-11-15 15:17:08 +0100

    Admin routes for posts

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -21,10 +21,10 @@ const Routes = () => {
         <Route path="/" page={HomePage} name="home" />
       </Set>
       <Set wrap={PostsLayout}>
-        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/posts" page={PostPostsPage} name="posts" />
+        <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/admin/posts" page={PostPostsPage} name="posts" />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
